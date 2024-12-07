fb7f1edbd3f26d7b342151e412d9108c0685fa31
rianraff <rianraffi@gmail.com>
2024-12-08 00:35:20 +0700

    Revert "add client GUI"
    This reverts commit 1f55f3897901e582022097962d05482c5607b4fe.

--- Frontend/src/utils/dummy/roomData.js
+++ Frontend/src/utils/dummy/roomData.js
@@ -2,13 +2,13 @@ export const rooms = [
     {
         id: 1,
         name: 'Meeting Room',
-        image: '1.jpg',
+        image: 'http://fakeimg.pl/400x300',
         capacity: 10
     },
     {
         id: 2,
         name: 'Gym Room',
-        image: '2.jpg',
+        image: 'http://fakeimg.pl/400x300',
         capacity: 20
     }
 ];
